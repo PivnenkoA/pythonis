171aefbe731b79f609e31af8b0754bbbd61c23c6
Rune <runesmaker@yandex.ru>
2018-10-07 22:04:37 +0300

    Поправки

--- html/git.php
+++ html/git.php
@@ -6,17 +6,22 @@
 ?>
 </head>
 <body>
-
-<?php
-    include "../include/header.html";
-    include "../include/git/gitnav.html";
-?>
-<div class = "container">
-<?php
-    include "../include/git/startToGit.html";
-    include "../include/git/gitcommit.html";
-    include "../include/git/github.html";
-?>
-</div>
+	<div class = "container">
+		<div class = "row">
+			<div class = "col-lg-3 ">
+				<?php
+					include "../include/header.html";
+				?>
+			</div>
+			<div class = "col-lg-9 ">
+				<?php
+					include "../include/git/gitnav.html";
+					include "../include/git/startToGit.html";
+					include "../include/git/gitcommit.html";
+					include "../include/git/github.html";
+				?>
+			</div>
+		</div>
+	</div>
 <a href = "#nav">На верх</a>
 </body>
