5962a40464b4710d050785b3061d1411946c7141
Rune <runesmaker@yandex.ru>
2018-10-07 14:28:34 +0300

    Начало положенно!

---
+++ html/git.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>Справочник юного ПИТОНИСТА</title>
+<meta charset = "utf-8">
+</head>
+<body>
+<header>
+<h1>О GIT</h1>
+<ul>
+<li><a href = #>Git</a></li>
+<li>Html</li>
+<li>PYTHON</li>
+</ul>
+</header>
+<?php
+    include "../include/startToGit.html";
+    include "../include/gitcommit.html";
+?>
+
+</body>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>Справочник юного ПИТОНИСТА</title>
+<meta charset = "utf-8">
+</head>
+<body>
+	<header>
+		<h1>Справочник юного ПИТОНИСТА</h1>
+		<ul>
+			<li><a href = "html/git.php">Git</a></li>
+			<li>Html</li>
+			<li>PYTHON</li>
+		</ul>
+	</header>
+	<?php 
+		echo "<h3>Hello world!!!</h3>"
+	?>
+	<p>
+		Этот вэб-сайт является моей личной напоминалкой. Но любой желающий может воспользоваться
+		находящейся на нем информацией. Удачи!
+	</p>
+
+</body>
+
+
+
+</html>
